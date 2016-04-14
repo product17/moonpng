bdbc706d06e4e621f50a2702a9ec84308fab3e0c
Brent Crocker <crocker.brent@gmail.com>
2016-04-13 21:47:54 -0700

    removing mongo

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const mongoose = require('mongoose');
 require('node-jsx').install({ extension: '.jsx' });
 
 // Setup Database connection
-const db = mongoose.connect('mongodb://localhost/task-react', (err) => {
-  if (err) {
-    console.error(chalk.red('Could not connect to MongoDB!'));
-    console.log(chalk.red(err));
-  }
-});
+// const db = mongoose.connect('mongodb://localhost/task-react', (err) => {
+//   if (err) {
+//     console.error(chalk.red('Could not connect to MongoDB!'));
+//     console.log(chalk.red(err));
+//   }
+// });
 
 // Init the express application
-const app = require('./lib/configs/express-config')(db);
+const app = require('./lib/configs/express-config')();
 
 // Run the passport configs
 require('./lib/configs/passport-config')();
